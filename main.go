64b69e094c44525d50029e39884dacb61b15acd1
fjybiocs <87828006+fjybiocs@users.noreply.github.com>
2024-09-17 23:15:15 +0800

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go listen(ctx)
+
+	<-ctx.Done()
+}
+
+func listen(ctx context.Context) {
+	for {
+		var input string
+		println("> [" + time.Now().Format("2006-01-02 15:04:05") + "] Please enter something:")
+		_, _ = fmt.Scanln(&input)
+		println("[" + time.Now().Format("2006-01-02 15:04:05") + "] You entered: " + input)
+
+		if input == "exit" {
+			ctx.Done()
+			break
+		}
+	}
+}
